778e58bdb2f6f79463b310260adfac7130754b51
Marcos Domingues <dfenixweb@gmail.com>
2021-08-10 10:15:51 -0300

    teste

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose.connect(
 );
 
 const corsOptions = {
-  origin: "https://app-reacttest.herokuapp.com/personagens",
+  origin: "https://app-reacttest.herokuapp.com/",
   optionsSuccessStatus: 200,
 };
 
